42d8b562d4d4b2ee0557da63379471a7790b4609
RocioBLion <rociobelenlion@hotmail.com>
2022-10-26 14:22:12 -0300

    add datetime

--- my_app/models.py
+++ my_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import datetime, date
 
 class Member(models.Model):
     name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
+    birth_date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
     
     
     def __str__(self):
-        return f"{self.name}  {self.last_name} - {self.age} years old"
+        return f"{self.name}  {self.last_name} - {self.age} years old. Today's date: {str(self.birth_date)}"
+    
+    
